33a4052fc888fb2447cfb0d06769b616cbb0ac98
Daniel Dearlove <dan@yorkmakerhub.com>
2016-11-06 14:14:02 +0000

    Added all other sensors from the SenseHat

--- measure_environment.py
+++ measure_environment.py
@@ -4,3 +4,36 @@ from sense_hat import SenseHat
 sense = SenseHat()
 sense.clear()
 
+humidity = sense.get_humidity()
+print("Humidity: %s %%rH" % humidity)
+print(sense.humidity)
+
+temp1 = sense.get_temperature()
+print("Temperature: %s C" % temp1)
+print(sense.temp)
+print(sense.temperature)
+
+temp2 = sense.get_temperature_from_pressure()
+print("Temperature: %s C" % temp2)
+
+pressure = sense.get_pressure()
+print("Pressure: %s Millibars" % pressure)
+print(sense.pressure)
+
+orientation = sense.get_orientation_radians()
+print("Orientationn p: {pitch}, r: {roll}, y: {yaw}".format(**orientation))
+print(sense.orientation_radians)
+
+compass = sense.get_compass_raw()
+print("Compass x: {x}, y: {y}, z: {z}".format(**compass))
+print(sense.compass_raw)
+
+gyro = sense.get_gyroscope_raw()
+print("Gyroscope x: {x}, y: {y}, z: {z}".format(**gyro))
+print(sense.gyro)
+print(sense.gyroscope)
+
+accel = sense.get_accelerometer_raw()
+print("Accelerometer x: {x}, y: {y}, z: {z}".format(**accel))
+print(sense.accel)
+print(sense.accelerometer)
